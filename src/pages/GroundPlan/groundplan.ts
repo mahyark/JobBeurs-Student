3b1470bb3849a193831db68dfe3e5ab85522a514
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2017-12-12 23:04:58 +0100

    plattegrond fix

--- src/pages/GroundPlan/groundplan.ts
+++ src/pages/GroundPlan/groundplan.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'groundplan-ionic',
   templateUrl: 'groundplan.html'
 })
 export class GroundPlan {
-  constructor() {
+	constructor(public toastCtrl: ToastController) {
+		this.presentToast();
+	}
+	public tap: number = 600;
 
-  }
+	tapEvent(e) {
+	    if (this.tap != 2000) {
+	        this.tap = 2000;
+	    }
+	    else
+	        this.tap = 600;
+	}
+
+	presentToast() {
+	    let toast = this.toastCtrl.create({
+	      position: 'top',
+	      message: 'Tap op het plattegrond om te zoomen',
+	      duration: 6000
+	    });
+	    toast.present();
+  	}
 }

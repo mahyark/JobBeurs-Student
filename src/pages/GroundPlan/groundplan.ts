3e53677e5d5bb9030aa5a016979d9d46a0b94305
Mahyar Moghimi Kian <mahyar.kian@gmail.com>
2017-12-12 23:09:26 +0100

    margin fix

--- src/pages/GroundPlan/groundplan.ts
+++ src/pages/GroundPlan/groundplan.ts
@@ -9,14 +9,14 @@ export class GroundPlan {
 	constructor(public toastCtrl: ToastController) {
 		this.presentToast();
 	}
-	public tap: number = 600;
+	public tap: number = 400;
 
 	tapEvent(e) {
-	    if (this.tap != 2000) {
-	        this.tap = 2000;
+	    if (this.tap != 800) {
+	        this.tap = 800;
 	    }
 	    else
-	        this.tap = 600;
+	        this.tap = 400;
 	}
 
 	presentToast() {

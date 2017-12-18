3307415a14147cdfd4934d19f1448dacb10d1d21
StijnDefour <defourstijn10@gmail.com>
2017-12-18 16:17:52 +0100

    Bedrijven toegevoegd

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,33 +1,45 @@
 import { Component } from '@angular/core';
-
 import { NavController, NavParams } from 'ionic-angular';
-
 import { ItemDetailsPage } from '../item-details/item-details';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
+
 export class ListPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{Bedrijf: string, E_mail: string, Contact: string, Adres: string, Post: string, Gemeente: string, Telefoon: string, Standnr: number, Sector: string, Opleiding: string, groen: string, geel: string, rood: string, blauw: string, wit: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.initializeItems();
   }
 
   initializeItems() {
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.getBedrijven();
+    this.items = JSON.parse(localStorage.getItem('bedrijven'));
+  }
+
+  async getBedrijven() {
+    var url_s = 'https://defourstijn.cloudant.com/bedrijven/65377d850d363f7336e28f83f0a0729b';
+		$.get(
+				url_s,
+				function(data_o) {
+          this.items = data_o.bedrijven;
+          for(let i = 0; i < data_o.bedrijven.length; i++) {
+            var Opleidingen = new Array();
+            if (this.items[i].groen=="WAAR"){Opleidingen.push("Bedrijfsmanagement Office Management Communicatie")};
+            if (this.items[i].geel=="WAAR"){Opleidingen.push("ICT Multimedia Grafische en Digitale Media")};
+            if (this.items[i].rood=="WAAR"){Opleidingen.push("Wetenschappen Techniek")};
+            if (this.items[i].blauw=="WAAR"){Opleidingen.push("Toegepaste Informatica Electronica ICT")};
+            if (this.items[i].wit=="WAAR"){Opleidingen.push("Gezondheid Onderwijs")};
+
+            this.items[i].Opleiding = Opleidingen.toString();
+            localStorage.setItem('bedrijven', JSON.stringify(this.items));
+          }
+				}
+			);
   }
 
   itemTapped(event, item) {
@@ -46,7 +58,7 @@ export class ListPage {
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.items = this.items.filter((item) => {
-        return (item.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        return (item.Bedrijf.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
   }
